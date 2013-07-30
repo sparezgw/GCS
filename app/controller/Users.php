38e975c02288601d6e0fe157ae90c0830f5ecc2f
Guowei Zhao <zhaoguowei@foxmail.com>
2013-07-31 00:06:47 +0800

    Add Register
    view & controller

--- app/controller/Users.php
+++ app/controller/Users.php
@@ -19,20 +19,22 @@ class Users extends Controller {
 
 		if ($f3->exists('POST.login')) {
 
-			$this->user->load(array('uname=?', $f3->get('POST.username')));
+			$u = $this->user;
 
-		    if(empty($this->user->uID)) //username does not exist.
+			$u->load(array('uname=?', $f3->get('POST.username')));
+
+		    if(empty($u->uID)) //username does not exist.
 
 		    	$msg = "用户名不存在。";
 
-		    else if ($f3->get('POST.password') != $this->user->passwd) //password is wrong.
+		    else if ($f3->get('POST.password') != $u->passwd) //password is wrong.
 
 		    	$msg = "密码输入有误。";
 
 		    else { //both username and password are right, and goto main page.
 
-				$f3->set('SESSION.UUID', $this->user->uID);
-				$f3->set('SESSION.PID', $this->user->pID);
+				$f3->set('SESSION.UUID', $u->uID);
+				$f3->set('SESSION.PID', $u->pID);
 				$f3->reroute('/home');
 			}
 
@@ -58,4 +60,51 @@ class Users extends Controller {
 		$f3->reroute('/user/login');
 	}
 
+	function reg($f3) {
+	    	
+		if ($f3->exists('POST.reg')) {
+			$u = $this->user;
+			$email = $f3->get('POST.username');
+			$name = $f3->get('POST.name');
+			$u->load(array('uname=?', $email));
+			if ($f3->get('POST.password') != $f3->get('POST.confirm'))
+				$msg = "密码输入两次不一样。";
+			elseif (!empty($this->user->uID))
+				$msg = "用户名已存在。";
+			else {
+				$u->uname = $email;
+				$u->passwd = $f3->get('POST.password');
+				$u->pID = '000000';
+				$u->insert();
+
+				$u->load(array('uname=?', $email));
+				$uid = $u->uID;
+
+				$p = $this->db->exec(
+					array(
+						'INSERT INTO people (uID,name,email) VALUES (:uid,:name,:email)',
+						'SELECT pID FROM people WHERE uID=:uid'
+					),
+					array(
+						array(':uid'=>(int)$uid, ':name'=>$name, ':email'=>$email),
+						array(':uid'=>(int)$uid)
+					)
+				);
+				$u->load(array('pID=?', '000000'));
+				$u->pID = $p[0]['pID'];
+				$u->update();
+
+				$f3->reroute('/');
+				
+			}
+			$f3->set('msg', $msg);
+			$f3->set('pageTitle', 'Register');
+			$f3->set('pageContent', 'users/_reg.html');
+			
+		} else {
+			$f3->set('pageTitle', 'Register');
+			$f3->set('pageContent', 'users/_reg.html');
+		}
+		
+	}
 }
--- sql/GCS.sql
+++ sql/GCS.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2013 年 07 月 27 日 00:02
+-- 生成日期: 2013 年 07 月 31 日 00:01
 -- 服务器版本: 5.5.28
 -- PHP 版本: 5.3.15
 
@@ -23,6 +23,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `Cards`
+--
+
+CREATE TABLE IF NOT EXISTS `Cards` (
+  `cID` smallint(6) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `pID` smallint(6) unsigned zerofill NOT NULL,
+  `uID` tinyint(4) unsigned zerofill NOT NULL,
+  `family` varchar(100) DEFAULT NULL,
+  `create_time` datetime NOT NULL,
+  `updated_time` datetime DEFAULT NULL,
+  `next_time` datetime DEFAULT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT '0',
+  `memo` tinytext,
+  PRIMARY KEY (`cID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `Notes`
+--
+
+CREATE TABLE IF NOT EXISTS `Notes` (
+  `nID` smallint(8) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `cID` smallint(6) unsigned zerofill NOT NULL,
+  `uID` tinyint(4) unsigned zerofill NOT NULL,
+  `time` datetime NOT NULL,
+  `mode` tinyint(1) unsigned zerofill NOT NULL DEFAULT '0',
+  `content` tinytext,
+  PRIMARY KEY (`nID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `People`
 --
 
@@ -30,10 +65,10 @@ CREATE TABLE IF NOT EXISTS `People` (
   `pID` smallint(6) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `uID` tinyint(4) unsigned zerofill NOT NULL,
   `parentID` smallint(6) unsigned zerofill NOT NULL DEFAULT '000000',
-  `name` varchar(9) NOT NULL,
+  `name` varchar(20) NOT NULL,
   `gender` tinyint(1) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
-  `income` varchar(9) DEFAULT NULL,
+  `income` varchar(20) DEFAULT NULL,
   `company` varchar(50) DEFAULT NULL,
   `position` varchar(30) DEFAULT NULL,
   `address` varchar(100) DEFAULT NULL,
@@ -50,12 +85,12 @@ CREATE TABLE IF NOT EXISTS `People` (
 --
 
 INSERT INTO `People` (`pID`, `uID`, `parentID`, `name`, `gender`, `birthday`, `income`, `company`, `position`, `address`, `mobile`, `workphone`, `email`, `from`) VALUES
-(000001, 0001, 000000, '赵国维', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'sparezgw@qq.com', NULL),
+(000001, 0001, 000000, '赵国维', 1, NULL, '', '', '', '', '', '', 'sparezgw@qq.com', NULL),
 (000002, 0001, 000001, '程谟思', 0, '1985-03-08', NULL, '城市快报', NULL, NULL, '18622212345', NULL, 'chengmosi@foxmail.com', NULL),
 (000003, 0001, 000000, '张三', 1, '1988-08-08', '25万', '中宏保险天津分公司', '高级营业经理', '天津市和平区河北路君隆广场B2座10层', '13802254321', '022-68579990-8681', 'zhaoguowei@foxmail.com', 3),
-(000004, 0001, 000000, '李四', 1, '1984-09-12', 'high', '', '', '', '18900022222', '', 'abc@abc.com', 0),
+(000004, 0001, 000000, '李四', 0, '1992-09-12', 'high', '', '', '', '18900022222', '', 'abc@abc.com', 0),
 (000005, 0001, 000000, '王五', 0, '2000-10-10', '', '', '', '', '', '', '', 1),
-(000006, 0001, 000000, '王自健', 1, '1982-06-06', '50万', '东方卫视', '主持人', '', '13902066666', '', 'wangzijian@126.com', 3);
+(000006, 0001, 000000, '王自健', 1, NULL, '50万', '东方卫视', '主持人', '上海市浦东新区', '13902066666', '', 'wangzijian@126.com', 3),
 
 -- --------------------------------------------------------
 
@@ -80,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `Users` (
 --
 
 INSERT INTO `Users` (`uID`, `uname`, `passwd`, `pID`) VALUES
-(0001, 'abc', 'abc', 000001);
+(0001, 'abc', 'abc', 000001),
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
