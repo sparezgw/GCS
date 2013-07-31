de4fc03ff21fadd1859ac7c79656acd3b3c3fa4b
Guowei Zhao <zhaoguowei@foxmail.com>
2013-08-01 00:15:54 +0800

    continue with card controller

--- app/controller/Cards.php
+++ app/controller/Cards.php
@@ -15,27 +15,33 @@ class Cards extends Controller {
 	}
 
 	function show($f3, $args) {
-	    	
-		$p = $this->people;
 		$c = $this->card;
-		$pid = (empty($args['pid']))?'':$args['pid'];
+		$pid = (int)(empty($args['pid']))?'':$args['pid'];
+		$uid = (int)$f3->get('SESSION.UUID');
 
-		$p->load(array('pID=?', $pid));
-		$p->copyto('person');
-
-		$c->load(array('pID=?', $pid));
-		if ($c->dry()) {
-			$f3->set('family',
-				$p->select('pID,name', array('parentID=?', $pid))
-			);
-			$f3->set('people',
-				$p->select('*', 'uID='.(int)$f3->get('SESSION.UUID').' and parentID=0 and pID<>'.(int)$pid)
-			);
-			$f3->set('pageTitle', 'New Card');
-			$f3->set('pageContent', 'cards/_show.html');
+		if ($f3->exists('POST.cardAdd')) {
+			$family = $f3->split($f3->get('POST.family'));
+			$f3->set('POST.family', json_encode($family));
+			
 		} else {
-			$f3->set('pageTitle', 'Card '.$pid);
-			$f3->set('pageContent', 'cards/_show.html');
+			$p = $this->people;
+			$p->load(array('pID=?', $pid));
+			$p->copyto('person');
+
+			$c->load(array('pID=?', $pid));
+			if ($c->dry()) {
+				$f3->set('family',
+					$p->select('pID,name', array('parentID=?', $pid))
+				);
+				$f3->set('people',
+					$p->select('*', 'uID='.$uid.' and parentID=0 and pID<>'.$pid)
+				);
+				$f3->set('pageTitle', 'New Card');
+				$f3->set('pageContent', 'cards/_show.html');
+			} else {
+				$f3->set('pageTitle', 'Card '.$pid);
+				$f3->set('pageContent', 'cards/_show.html');
+			}
 		}
 	}
 }
--- js/main.js
+++ js/main.js
@@ -3,31 +3,55 @@ $(document).on("pageinit", function() {
         var content = "<li><a href='#'>Ferrari</a></li>";
         $("#set").append( content ).listview('refresh');
     });
-    // $(".mem").click(function() {
-        
-    //     var $item = $(this);
-    //     // $(this).prop({disabled: true});
-    //     // $(this).removeClass("mem");
-    //     var content = "<li><a href='" + $item.attr('data-href') + "'>" + $item.html() + "</a></li>";
-        
-        
-    //     $("#member").append( content ).listview('refresh');
-    //     // $(this).parent("li").remove();
-    //     $("#all").listview('refresh');
-    // });
 
-    $("#all li").on( "click", function() {
+    $("#allClient li").on( "click", function() {
         var $item = $(this);
-        var content = "<li><a href='" + $item.attr('data-href') + "'>" + $item.text() + "</a></li>";
-        $("#member").append(content).listview('refresh');
-        $(this).remove();
-        // $(this).css( "display", "none" );
-        $("#all").listview('refresh');
+        var pid = $item.attr('data-id');
+        var content = "<li data-id='" + pid + "'><a href='/client/" + pid + "'>" + $item.text() + "</a><a href='javascript:remove(" + pid + ")' /></li>";
+        $("#familyMember").append(content).listview('refresh');
+        $(this).css( "display", "none" );
+        $("#allClient").listview('refresh');
     });
+    
     $("#expand").click(function() {
         $("#set").children(":last").trigger( "expand" );
     });
     $("#collapse").click(function() {
         $("#set").children(":last").trigger( "collapse" );
     });
+
+    $('button[name="cardAdd"]').on("click", function() {
+        var fam = $("#familyMember li").map(function() {
+            var li = $.makeArray(this);
+            return $(li).attr("data-id");
+        }).get().join();
+        $('input[name="family"]').val(fam);
+    })
 });
+
+function remove(pid) {
+    $familyItem = $("#familyMember li[data-id='" + pid + "']");
+    var name = $.trim($familyItem.children().text());
+    $familyItem.css( "display", "none" );
+    $("#familyMember").listview('refresh');
+
+    $clientItem = $("#allClient li[data-id='" + pid + "']");
+    if ($clientItem.is("li")) {
+        $clientItem.css( "display", "list-item" );
+    } else{
+        var content = '<li data-icon="plus" data-id="'+pid+'"><a href="javascript:back('+pid+')">'+name+'</a></li>';
+        $("#allClient li:first").after(content);
+    };
+    
+    $("#allClient").listview('refresh');
+    
+}
+function back(pid) {
+    $clientItem = $("#allClient li[data-id='" + pid + "']");
+    $clientItem.remove();
+    $("#allClient").listview('refresh');
+
+    $familyItem = $("#familyMember li[data-id='" + pid + "']");
+    $familyItem.css( "display", "list-item" );
+    $("#familyMember").listview('refresh');
+}
